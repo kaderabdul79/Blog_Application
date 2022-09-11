3e5279ed40cdcc9571fc074b5f84ce31df3bb416
Abdul Kader <80806477+kaderabdul79@users.noreply.github.com>
2022-09-12 04:45:31 +0600

    installed laravel,vue,vue-router and setup vue, also solved the route issue

---
+++ resources/js/layouts/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    Lorem ipsum dolor sit amet.
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ resources/js/views/Home.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    Home {{name}}
+  </div>
+</template>
+
+<script>
+import { onMounted, ref } from 'vue';
+import axios from 'axios';
+
+export default {
+  setup(){
+    const name = ref("hasan al kadir")
+    async function checkTest(){
+       const a = await axios.get('/test')
+      .then(res=>console.log(res))
+      .then(error => console.log(error))
+
+      return a;
+    }
+    onMounted(()=>{
+      checkTest()
+    })
+    return {name}
+  }
+}
+</script>
+
+<style>
+
+</style>
