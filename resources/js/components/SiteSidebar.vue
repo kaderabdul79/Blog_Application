b0d32e9a7d978bc1a40f7309ad7a00371e5bfe9c
Abdul Kader <80806477+kaderabdul79@users.noreply.github.com>
2022-09-14 04:45:31 +0600

    In siebar add toggle button, for mobile menu

--- resources/js/components/SiteSidebar.vue
+++ resources/js/components/SiteSidebar.vue
@@ -1,37 +1,72 @@
 <template>
-    <!-- sidebar -->
-    <div class="sidebar">
-    <span class="closeButton">&times;</span>
-    <p class="brand-title"><router-link :to="{name: 'Home'}">Laravel with Vue Blog</router-link></p>
+  <!-- sidebar -->
+    <div class="sidebar" :class="{ showOverlay: overlayVisibility }">
+      <span class="closeButton" @click="hideOverlay">&times;</span>
+      <p class="brand-title"><a href="">Laravel with Vue Blog</a></p>
 
-    <div class="side-links">
+      <div class="side-links">
         <ul>
-        <li><router-link class="active" :to="{name: 'Home'}">Home</router-link></li>
-        <li><router-link :to="{name: 'Blog'}">Blog</router-link></li>
-        <li><router-link :to="{name: 'About'}">About</router-link></li>
-        <li><router-link :to="{name: 'Contact'}">Contact</router-link></li>
+          <li>
+            <router-link @click="hideOverlay" :to="{ name: 'Home' }"
+              >Home</router-link
+            >
+          </li>
+          <li>
+            <router-link @click="hideOverlay" :to="{ name: 'Blog' }"
+              >Blog</router-link
+            >
+          </li>
+          <li>
+            <router-link @click="hideOverlay" :to="{ name: 'About' }"
+              >About</router-link
+            >
+          </li>
+          <li>
+            <router-link @click="hideOverlay" :to="{ name: 'Contact' }"
+              >Contact</router-link
+            >
+          </li>
         </ul>
-    </div>
-
-        <!-- sidebar footer -->
-        <footer class="sidebar-footer">
-          <div>
-            <a href=""><i class="fab fa-facebook-f"></i></a>
-            <a href=""><i class="fab fa-instagram"></i></a>
-            <a href=""><i class="fab fa-twitter"></i></a>
-          </div>
-
-          <small>&copy; 2022 Laravel & Vue Blog</small>
-        </footer>
       </div>
-</template>
 
+      <!-- sidebar footer -->
+      <footer class="sidebar-footer">
+        <div>
+          <a href=""><i class="fab fa-facebook-f"></i></a>
+          <a href=""><i class="fab fa-instagram"></i></a>
+          <a href=""><i class="fab fa-twitter"></i></a>
+        </div>
+
+        <small>&copy; 2022 Laravel with Vue Blog</small>
+      </footer>
+    </div>
+    <!-- Menu Button -->
+    <div class="menuButton" @click="ShowOverlay">
+      <div class="bar"></div>
+      <div class="bar"></div>
+      <div class="bar"></div>
+    </div>
+</template>
 <script>
 export default {
-
-}
+  data() {
+    return {
+      overlayVisibility: false,
+    };
+  },
+  methods: {
+    ShowOverlay() {
+      this.overlayVisibility = true;
+    },
+    hideOverlay() {
+      this.overlayVisibility = false;
+    },
+  },
+};
 </script>
-
-<style>
-
+<style scoped>
+.showOverlay {
+  width: 100%;
+  z-index: 5;
+}
 </style>
