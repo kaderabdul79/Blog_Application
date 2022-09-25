b52bd582ef1cd8c5463537922923e95ce32caf91
Abdul Kader <80806477+kaderabdul79@users.noreply.github.com>
2022-09-26 04:33:31 +0600

    create add new post layout,upload image and preview and write logic in controller

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController;
 use App\Http\Controllers\CategoryController;
 use Laravel\Fortify\Http\Controllers\RegisteredUserController;
 use Laravel\Fortify\Http\Controllers\AuthenticatedSessionController;
@@ -12,7 +13,7 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 Route::post('register', [RegisteredUserController::class, 'store']);
-Route::post('login', [AuthenticatedSessionController::class, 'store']);
+
 
 // categories
 Route::middleware('auth:sanctum')->post('categories/create', [CategoryController::class, 'store']);
@@ -20,5 +21,8 @@ Route::get('categories', [CategoryController::class, 'index']);
 Route::middleware('auth:sanctum')->get('categories/{category}', [CategoryController::class, 'show']);
 Route::middleware('auth:sanctum')->put('categories/{category}', [CategoryController::class, 'update']);
 Route::middleware('auth:sanctum')->delete('categories/{category}', [CategoryController::class, 'destroy']);
+
+// posts
+Route::middleware('auth:sanctum')->post('posts', [PostController::class, 'store']);
 
 Route::middleware('auth:sanctum')->post('logout', [AuthenticatedSessionController::class, 'destroy']);
